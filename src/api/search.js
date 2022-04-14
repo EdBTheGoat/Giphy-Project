a21f4adeeeac080a6b5f7c0b007817899c985252
Edison Blum <blum.edison12@gmail.com>
2022-04-14 11:37:08 -0400

    ENV for api keys

--- src/api/search.js
+++ src/api/search.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
+const SEARCH_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+console.log(SEARCH_KEY);
 
 export default axios.create({
-	baseURL: 'https://api.giphy.com/v1/gifs/search?api_key=wi6R97bUDMiAQjBUFDi4aYIvRat4TYS4&limit=25',
+	baseURL: `https://api.giphy.com/v1/gifs/search?api_key=${SEARCH_KEY}&limit=25`,
 });
--- src/api/trending.js
+++ src/api/trending.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
+const TRENDING_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
 export default axios.create({
-	baseURL: 'https://api.giphy.com/v1/gifs/trending?api_key=wi6R97bUDMiAQjBUFDi4aYIvRat4TYS4&limit=25',
+	baseURL: `https://api.giphy.com/v1/gifs/trending?api_key=${TRENDING_KEY}&limit=25`,
 });
